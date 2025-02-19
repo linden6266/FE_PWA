fc3760c41b41f7bae9b89765ed99b7e3fa065d9d
linden6266 <tvdlinden127@gmail.com>
2025-02-19 12:09:24 +0100

    done with the routing

---
+++ src/env.js
@@ -0,0 +1,7 @@
+export default {
+    VUE_APP_IS_PWA: true,
+    VUE_APP_GUESTPAGE_URL: process.env.VUE_APP_GUESTPAGE_URL,
+    VUE_APP_MENU_URL: process.env.VUE_APP_MENU_URL,
+    VUE_APP_GROUPCODE_URL: process.env.VUE_APP_GROUPCODE_URL,
+};
+
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,9 @@
+const Dotenv = require("dotenv-webpack");
 const path = require("path");
 const { VueLoaderPlugin } = require("vue-loader");
 const { ModuleFederationPlugin } = require("webpack").container;
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { DefinePlugin } = require("webpack");
 
 module.exports = {
     mode: "development",
@@ -36,18 +38,29 @@ module.exports = {
         ],
     },
     plugins: [
+        new Dotenv(), // ✅ Voeg Dotenv-plugin toe om .env-bestand in te laden
         new ModuleFederationPlugin({
-            name: "host",
+            name: "pwamenu",
+            filename: "pwamenuEntry.js",
+            exposes: {
+                "./env": "./src/env" // ✅ Expose the env module
+            },
             remotes: {
                 guestpage: "guestpage@http://localhost:8082/guestpageEntry.js",
                 remote: `remote@http://localhost:8081/remoteEntry.js`,
                 groupcode: "groupcode@http://localhost:8084/groupcodeEntry.js",
             },
+            shared: {
+                vue: { singleton: true, eager: true, requiredVersion: "^3.2.0" },
+                "vue-router": { singleton: true, eager: true, requiredVersion: "^4.0.0" },
+                "vue-i18n": { singleton: true, eager: true, requiredVersion: "^9.0.0" },
+                axios: { singleton: true, eager: true },
+            }
+
         }),
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
             template: "./public/index.html",
         }),
-
     ],
 };
