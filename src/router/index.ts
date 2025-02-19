873bc640c1aa1bca5e62d7622c23696345af6f98
linden6266 <tvdlinden127@gmail.com>
2025-02-19 14:24:16 +0100

    fixed the pwa

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 const MenuApp = () => import('remote/bootstrap');
 const guestpageApp = () => import('guestpage/main');
 const groupcodeApp = () => import('groupcode/main');
+const HomeView = () => import('remote/HomeView');
 // const isPWA = process.env.VUE_APP_USE_PWA === 'true';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/menu', component: MenuApp },
-    { path: '/groupcode', component: groupcodeApp },
+    { path: "/groupcode", component: groupcodeApp },
+    { path: '/', redirect: '/guestpage' },
+    { path: '/guestpage', component: HomeView },
     { path: '/:code', component: guestpageApp },
   ]
 })
